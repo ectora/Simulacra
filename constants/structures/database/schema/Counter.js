02d17257fee1f3eb7f83800303753c7c452bab36
No one <106720995+Corsaekyo@users.noreply.github.com>
2022-06-02 11:51:09 +0800

    makes room for empty template

--- constants/structures/database/schema/Counter.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model("counter", new mongoose.Schema({
-      counterName: { type: String, required: true },
-      count: { type: Number, required: true }
-}));
